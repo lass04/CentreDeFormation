ed086d7b2300ca69fbf4165d4ebe860bddf089b3
lass04 <yassinelassoued58@gmail.com>
2025-12-05 15:21:28 +0100

    Added Candidats Display On private Space

--- src/app/private-space/admin-login/admin-login.ts
+++ src/app/private-space/admin-login/admin-login.ts
@@ -3,7 +3,7 @@ import { DataService } from '../../services/data-service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -12,13 +12,16 @@ import { Component } from '@angular/core';
   templateUrl: './admin-login.html',
   styleUrl: './admin-login.css',
 })
-export class AdminLogin {
+export class AdminLogin implements OnInit{
  
   username="";
   password="";
 
   constructor(private dataSvc:DataService,private authSvc:Authservice,private router:Router){}
 
+  ngOnInit(){
+    this.authSvc.logout();
+  }
   onSubmit(){
    if(this.authSvc.verify(this.username,this.password)){
     this.router.navigate(['/private-space/home']);
--- src/app/private-space/candidats/candidats.ts
+++ src/app/private-space/candidats/candidats.ts
@@ -1,22 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Privatenavbar } from './../../components/privatenavbar/privatenavbar';
+import { DataService } from './../../services/data-service';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Candidat } from '../../interfaces/candidat';
+import { candidats } from '../../lists-load/candidats-load';
 
 @Component({
   selector: 'app-candidats',
-  imports: [],
+  imports: [Privatenavbar,FormsModule,CommonModule],
   templateUrl: './candidats.html',
   styleUrl: './candidats.css',
 })
 export class Candidats implements OnInit{
 
-  constructor(private router:Router){}
+  CandidatToUpdate:Candidat=candidats[0];
+  ToUpdate:boolean=false;
+  candidats:Candidat[]=[];
+
+  constructor(private router:Router,private dataSvc:DataService){}
 
   ngOnInit(){
-    
+    this.candidats = this.dataSvc.GetCandidats();
+  }
+
+  // Suppression du candidat conçerné
+
+  Delete(id:number){
+    const CandidatName = this.dataSvc.GetCandidatById(id)?.nom; 
+    let confirmer = confirm("Etes vous sur de supprimer le candidat "+CandidatName);
+    if(confirmer){
+      this.dataSvc.DeleteCandidat(id);
+      this.router.navigate(['/private-space/cadidats']);
+    }
+  }
+
+  // Rechercher le candidat By Id , et affecter true à ToUpdate 
+  // pour rendre le formulaire visible
+  Update(id:number){
+     const Search = this.dataSvc.GetCandidatById(id);
+     if(Search){
+      this.CandidatToUpdate = Search;
+      this.ToUpdate=true; 
+    }
   }
 
   OnSubmit(){
-
+    this.dataSvc.UpdateCandidat(this.CandidatToUpdate);
+    this.ToUpdate=false;
+    console.log(this.CandidatToUpdate);
   }
 
 }
